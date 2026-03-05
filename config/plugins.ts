d92ec44e0b569e7b2ca20c7f6e67f2d9e4492db4
Ashish Zapadiya <ashishzapadiya834@gmail.com>
2026-03-05 22:31:45 +0530

    feat: configure GraphQL plugin with playground and introspection settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,12 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  graphql: {
+    config: {
+      playgroundAlways: true,
+      introspection: true,
+    },
+  },
+});
 
 export default config;
